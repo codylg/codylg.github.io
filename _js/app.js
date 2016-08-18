4ebb1e113fe7923db153266a4a3cc8a501c04eac
codylg <cody@scrunch.co>
2016-08-18 23:04:50 +1000

    Added app.js (empty)

---
+++ _js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  // Scroll to top
+  // backToTop.on('click', '.back-to-top-button', function() {
+  //   $('html, body').animate({ scrollTop: '0' });
+  // });
+
+});
