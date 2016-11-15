66378d6b7e006b71917a0d0302973451cce46519
codylg <clg@clg.name>
2016-11-15 21:55:24 +1000

    Clear app.js

--- _js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  // Scroll to top
-  // backToTop.on('click', '.back-to-top-button', function() {
-  //   $('html, body').animate({ scrollTop: '0' });
-  // });
-
-});
