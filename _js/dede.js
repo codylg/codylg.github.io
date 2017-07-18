c0c9274f2785553118c4772a90515ec180145f58
Cody Gordon <clg@clg.name>
2017-07-19 00:23:59 +1000

    Remove unneeded tests

--- _js/dede.js
+++ _js/dede.js
@@ -40,19 +40,11 @@ function clearTransform() {
 }
 
 
-var ball   = document.querySelector('.ball');
-var garden = document.querySelector('.garden');
 var output = document.querySelector('.output');
-
-var maxX = garden.clientWidth  - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
   var x = event.beta;  // In degree in the range [-180,180]
   var y = event.gamma; // In degree in the range [-90,90]
-
-  output.innerHTML  = "beta : " + x + "\n";
-  output.innerHTML += "gamma: " + y + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
@@ -63,14 +55,6 @@ function handleOrientation(event) {
   // x and y to [0,180]
   x += 90;
   y += 90;
-
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
-  ball.style.top  = (maxX*x/180 - 10) + "px";
-  ball.style.left = (maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
-
-output.innerHTML  = "beta : \n";
-output.innerHTML += "gamma: \n";
