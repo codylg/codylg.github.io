3768e56fcd9ac8421a1bac6c22b8de50c3a08799
Cody Gordon <clg@clg.name>
2017-07-18 19:42:39 +1000

    Generalised IDs

--- _js/dede.js
+++ _js/dede.js
@@ -22,19 +22,19 @@ function addTransform(event) {
 
   wrapper.style.transform = "rotateY(" + wrapperPx / 10 + "deg) rotateX(" + wrapperPy / -10 + "deg)";
   document.getElementById("exposure").style.transform = "translate(" + wrapperPx / -7 + "px, " + wrapperPy / -7 + "px)";
-  document.getElementById("bokeh-1").style.transform = "translate(" + wrapperPx / -1 + "px, " + wrapperPy / -2 + "px)";
-  document.getElementById("bokeh-2").style.transform = "translate(" + wrapperPx / -2 + "px, " + wrapperPy / -3 + "px)";
-  document.getElementById("bokeh-3").style.transform = "translate(" + wrapperPx / -3 + "px, " + wrapperPy / -4 + "px)";
-  document.getElementById("bokeh-4").style.transform = "translate(" + wrapperPx / -4 + "px, " + wrapperPy / -5 + "px)";
+  document.getElementById("depth-1").style.transform = "translate(" + wrapperPx / -5 + "px, " + wrapperPy / -6 + "px)";
+  document.getElementById("depth-2").style.transform = "translate(" + wrapperPx / -4 + "px, " + wrapperPy / -5 + "px)";
+  document.getElementById("depth-3").style.transform = "translate(" + wrapperPx / -3 + "px, " + wrapperPy / -4 + "px)";
+  document.getElementById("depth-4").style.transform = "translate(" + wrapperPx / -2 + "px, " + wrapperPy / -3 + "px)";
   document.getElementById("gloss").style.opacity = wrapperPy / -100;
 }
 
 function clearTransform() {
   document.getElementById("double-exposure-wrapper").style.transform = "none";
   document.getElementById("exposure").style.transform = "none";
-  document.getElementById("bokeh-1").style.transform = "none";
-  document.getElementById("bokeh-2").style.transform = "none";
-  document.getElementById("bokeh-3").style.transform = "none";
-  document.getElementById("bokeh-4").style.transform = "none";
+  document.getElementById("depth-1").style.transform = "none";
+  document.getElementById("depth-2").style.transform = "none";
+  document.getElementById("depth-3").style.transform = "none";
+  document.getElementById("depth-4").style.transform = "none";
   document.getElementById("gloss").style.opacity = 0;
 }
