f4bb9b80dbef1b6a6731875ea459b1ec364a3007
Cody Gordon <clg@clg.name>
2017-07-23 22:20:08 +1000

    Merge scripts

--- _js/dede.js
+++ _js/dede.js
@@ -38,3 +38,26 @@ function clearTransform() {
   document.getElementById("depth-4").style.transform = "none";
   document.getElementById("gloss").style.opacity = 0;
 }
+
+var output = document.querySelector('.output');
+
+function handleOrientation(event) {
+  var x = event.beta;  // Range of -180 to 180
+  var y = event.gamma; // Range of -90 to 90
+
+  // Adjust ranges to nearest 100
+  x = x / 0.9;
+  y = y / 0.9;
+
+  // Adjust range limits of x
+  if (x >  100) { x =  100};
+  if (x < -50) { x = -50};
+
+  document.getElementById("exposure").style.transform = "translate(" + y / (-7/2) + "px, " + x / (-7/2) + "px)";
+  document.getElementById("depth-1").style.transform = "translate(" + y / (-5/2) + "px, " + x / (-6/2) + "px)";
+  document.getElementById("depth-2").style.transform = "translate(" + y / (-4/2) + "px, " + x / (-5/2) + "px)";
+  document.getElementById("depth-3").style.transform = "translate(" + y / (-3/2) + "px, " + x / (-4/2) + "px)";
+  document.getElementById("depth-4").style.transform = "translate(" + y / (-2/2) + "px, " + x / (-3/2) + "px)";
+}
+
+window.addEventListener('deviceorientation', handleOrientation);
