527f10efcdf1310b162b2216469e5b46c8f8e1e4
Cody Gordon <clg@clg.name>
2017-08-06 10:37:55 +1000

    Add toggle for night mode

--- _js/app.js
+++ _js/app.js
@@ -1,6 +1,20 @@
 var d = new Date();
 var h = d.getHours();
+var nightModeSwitch = document.getElementById("night-mode-switch");
 
 if ((h > 21 || h < 5) && location.pathname == "/") {
   document.body.className = 'night-mode';
+  nightModeSwitch.checked = true;
+}
+
+function toggleNightMode() {
+  console.log('Clicky');
+
+  if (nightModeSwitch.checked) {
+    document.body.className = '';
+    console.log('Night mode off');
+  } else {
+    document.body.className = 'night-mode';
+    console.log('Night mode on');
+  }
 }
