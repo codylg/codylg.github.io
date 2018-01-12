e8a2d3d71cea65b14aca081fd36ed795c4a52b5d
Cody Gordon <clg@clg.name>
2018-01-12 15:42:03 +1000

    Use localStorage to retain night mode pref

--- _js/app.js
+++ _js/app.js
@@ -2,16 +2,40 @@ var d = new Date();
 var h = d.getHours();
 var nightModeSwitch = document.getElementById("night-mode-switch");
 
-if ((h > 23 || h < 4) && location.pathname == "/") {
+function nightModeOn() {
   document.body.className = 'night-mode';
-  nightModeSwitch.checked = true;
+}
+
+function nightModeOff() {
+  document.body.className = '';
+}
+
+// Turn on night mode at night automatically, unless it has been set manually
+if (location.pathname == "/") {
+
+  if (localStorage.getItem('nightModePref')) {
+    // The user has set a night mode preference
+
+    if (localStorage.getItem('nightModePref') == 'on') {
+      nightModeOn();
+      nightModeSwitch.checked = true;
+    } else if (localStorage.getItem('nightModePref') == 'off') {
+      nightModeOff();
+    }
+
+  } else if ((h > 23 || h < 4)) {
+    nightModeOn();
+    nightModeSwitch.checked = true;
+  }
 }
 
 function toggleNightMode() {
 
   if (nightModeSwitch.checked) {
-    document.body.className = '';
+    nightModeOff();
+    localStorage.setItem('nightModePref', 'off'); // User has manually set night mode off
   } else {
-    document.body.className = 'night-mode';
+    nightModeOn();
+    localStorage.setItem('nightModePref', 'on'); // User has manually set night mode on
   }
 }
