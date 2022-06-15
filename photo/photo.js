09b9b9ebbaea2ec09939c3906e87733adaf36fb0
Cody <clg@clg.name>
2022-06-15 21:54:44 +1000

    Remove console logging

--- photo/photo.js
+++ photo/photo.js
@@ -3,17 +3,11 @@ var touchScreen = false;
 window.addEventListener('touchstart', function() {
   touchScreen == true;
   scrollContainer.classList.remove("hijacked");
-  console.log("touched!")
 });
-
-
-
 
 const scrollContainer = document.querySelector(".photo-gallery");
 
 scrollContainer.addEventListener("wheel", (evt) => {
-
-  console.log("scrolling!")
 
   if (window.innerHeight < window.innerWidth) {
     if (!touchScreen) {
@@ -22,8 +16,6 @@ scrollContainer.addEventListener("wheel", (evt) => {
       evt.preventDefault();
       scrollContainer.scrollLeft += evt.deltaX;
       scrollContainer.scrollLeft += evt.deltaY;
-
-      console.log("scrolling sideways!")
     }
   }
 });
